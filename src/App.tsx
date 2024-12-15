f92cde39f25ca6529f71fd6161829fee3e7a8139
Yuki <diesunabe@gmail.com>
2024-12-15 22:45:15 +0900

    p.126

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,51 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Search from "./pages/Search";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
+  const [user, setUser] = useState({
+    name: "",
+    password: "",
+  });
+
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  console.log(loggedIn);
+
+  const handleRegister = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    localStorage.setItem("user-data", JSON.stringify(user));
+    setLoggedIn(true);
+  };
+
+  useEffect(() => {
+    const userData = JSON.parse(localStorage.getItem("user-data")!);
+    userData && setLoggedIn(true);
+  }, []);
+
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/search" element={<Search />} />
+        <Route path="*" element={<h1>ページがありません</h1>} />
+        <Route
+          path="/register"
+          element={
+            <Register
+              handleChange={handleChange}
+              handleRegister={handleRegister}
+            />
+          }
+        />
+        {loggedIn && <Route path="/search" element={<Search />} />}
       </Routes>
     </>
   );
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,31 @@
-const Register = () => {
-  return <h2>登録ページ</h2>;
+type RegisterProps = {
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleRegister: (e: React.FormEvent<HTMLFormElement>) => void;
+};
+
+const Register = ({ handleChange, handleRegister }: RegisterProps) => {
+  return (
+    <>
+      <h2>登録ページ</h2>
+      <form onSubmit={handleRegister}>
+        <input
+          onChange={handleChange}
+          type="text"
+          name="name"
+          placeholder="ユーザーネーム"
+          required
+        />
+        <input
+          onChange={handleChange}
+          type="text"
+          name="password"
+          placeholder="パスワード"
+          required
+        />
+        <button>登録</button>
+      </form>
+    </>
+  );
 };
 
 export default Register;
